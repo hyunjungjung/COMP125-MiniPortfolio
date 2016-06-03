8ca9a8977ad37128faf097ca9b907c3a73638ff3
hyunjungjung <hjung3@my.centennialcollege.ca>
2016-06-03 01:27:18 -0400

    Added logo and paragraphs

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,7 +3,7 @@
 /*
         Author:       Joanne Jung (StuentID: 300432364)
         File Name:    app.js
-        Description:  JavaScript file for COMP 125- Assignment 1 web application. 
+        Description:  JavaScript file for COMP 125- Assignment 1 - Mini Portfolio. 
 */
 
 
@@ -20,12 +20,12 @@
     var paragraphs = [];
     
     // data for my pages   
+    //index
+    paragraphs[0] = "<div class=indexFirstSentence'> Loves to meet new people, but very shy.<div> <br><br><br><br> If you want to know more about me Click >> <a href='about.html'>About Me</a> <br> If you want to see my projects Click >> <a href='projects.html'>Projects</a> <br> If you want to contact me Click >> <a href='contact.html'>Contact</a></div>";
     
-    paragraphs[0] = "<span class='firstSentence'> Welcome to Joanne's mini portfolio!</span> <br>hahahaha";
+    paragraphs[1] = "The first Three projects of mine will be added even though they are not good/pretty.";
     
-    paragraphs[1] = "Three projects of mine will be added even though they are not good/pretty.";
-    
-    paragraphs[2] = "asdfasdfasfasdfasdfasdfasdf";
+    paragraphs[2] = "<div class='indexSecondSentence'> Graduated from Ryerson University with Bachelor of Commerce, Business Technology Management <br> Currently is enrolled in Health Informatics Technology at Centennial College <br> ";
     
     
     
